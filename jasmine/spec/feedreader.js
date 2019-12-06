1c58f8a4088e8879524d9975890943a58f8a7dad
Jacob <Jtdawgzone@users.noreply.github.com>
2019-12-05 17:46:07 -0800

    Added all test cases

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,7 +1,7 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
 /* We're placing all of our tests within the $() function,
@@ -9,52 +9,80 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
+
+    // Test suite describing RSS Feed array tests
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * allFeeds variable has been defined and that it is not empty. 
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('urls are defined', () => {
+            allFeeds.forEach((feed) => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', () => {
+            allFeeds.forEach((feed) => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })
+        });
     });
 
+    // Test suite describing menu tests
+    describe('The menu', () => {
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* A test that ensures the menu element is
+         * hidden by default. 
          */
+        it('is hidden by default', () => {
+            expect(document.querySelector('.menu-hidden')).toBeDefined();
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('changes visibility when menu icon is clicked', () => {
+             var menuIcon = document.querySelector('.menu-icon-link');
+             menuIcon.click();
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+             expect(document.querySelector('.menu-hidden')).toBeNull();
+
+             menuIcon.click();
+
+             expect(document.querySelector('.menu-hidden')).toBeDefined();
+         });
+    }); 
+
+    // Test suite describing async feed loading
+    describe('Initial Entries', function () {
+        
+        // If browser doesn't support ES6 Promises return
+        if(!browserSupportsPromises()) {
+            return;
+        }
+
+        beforeEach(()=> {
+            // Choose third feed in allFeeds to load async
+            // Return means we await the Promise to resolve
+            return loadFeedAsync(2);
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +90,70 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('loads and completes its work', () =>
+        {
+             // Check if there is at least one entry in the loaded feed
+             let feedContainer = document.querySelector('.feed');
+             expect(feedContainer.children.length).toBeGreaterThan(0);
+        }); 
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    // Test suite describing async feed loading and that feeds switch
+    describe('New Feed Selection', () => {
+
+        // If browser doesn't support ES6 Promises return
+        if(!browserSupportsPromises()) {
+            return;
+        }
+        
+        beforeEach(()=> {
+            // Choose fourth feed in allFeeds to load async
+            // Return means we await the Promise to resolve
+            return loadFeedAsync(3);
+        });
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('replaces current feed with new feed\'s entries', () => {
+             // Check if there is at least one entry in the loaded feed
+             let feedContainer = document.querySelector('.feed');
+             expect(feedContainer.children.length).toBeGreaterThan(0);
+
+             // Store the first entry for comparison after feed refresh
+             // Chceck that the string has content
+             let firstEntry = feedContainer.children[0];
+             expect(firstEntry.innerText.length).not.toBe(0);
+
+             // Refresh the feed container with a new feed async
+             // in this case the first feed in allFeeds
+             // Return means we await the Promise to resolve
+             return loadFeedAsync(0).then(() => {
+                 // Check if there is at least one entry in the newly loaded feed
+                feedContainer = document.querySelector('.feed');
+                expect(feedContainer.children.length).toBeGreaterThan(0);
+
+                // Store new first entry and check the string has content
+                let newFirstEntry = feedContainer.children[0];
+                expect(newFirstEntry.innerText.length).not.toBe(0);
+
+                // Check that the old first entry does not match the new one
+                expect(firstEntry.innerText).not.toMatch(newFirstEntry.innerText);
+             });
+        });
+    });
+
+    // Loads a feed async given a feed id to load from allFeeds
+    function loadFeedAsync(feedId) {
+        return new Promise((resolve, reject) => {
+            loadFeed(feedId, resolve);
+          });
+    };
+
+    // Checks to see if Browser supports ES6 Promises
+    // from: https://jasmine.github.io/tutorials/async
+    function browserSupportsPromises() {
+        return typeof(Promise) !== "undefined";
+    }
 }());
